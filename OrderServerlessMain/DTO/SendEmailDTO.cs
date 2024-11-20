03d0560e366915525397fb225b355ee35c3e3fac
Nguyễn Hoàng Phúc <hoangphucseiza@gmail.com>
2024-11-21 03:36:27 +0700

    Change and Done Function OrderServerless

---
+++ OrderServerlessMain/DTO/SendEmailDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderServerlessMain.DTO
+{
+    public class SendEmailDTO
+    {
+        public string? Email { get; set; }
+
+        public string? FullName { get; set; }
+        public string? OrderId { get; set; }
+        public decimal? TotalPrice { get; set; }
+    }
+}
